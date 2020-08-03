3a7bf49b0145cb9c54ca26ec4228861977fbc99a
tilak30 <tilakbhansali3009@gmail.com>
2020-08-03 22:41:46 +0530

    single page applications part-2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
-
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
     function RenderDish({dish}) {
             return(
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
+                <div className="col-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg top src={dish.image} alt={dish.name} />
+                        <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
             );
     }
     
 
-    function RenderComments({comments}){
+    function RenderComments({comments}) {
         const cmnt = comments.map((comment) => {
             return(
                 <li key={comment.id}>
@@ -33,33 +35,42 @@ import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
             );
         });
 
+        if (comments != null)
+            return (
+                <div className="col-12 col-md-5 m-1">
+                    <h4> Comments </h4>
+                    <ul className="list-unstyled">
+                        {cmnt}
+                    </ul>
+                </div>
+            );
+        else
         return (
-            <div>
-                <h4> Comments </h4>
-                <ul className="list-unstyled">
-                    {cmnt}
-                </ul>
-            </div>
+            <div></div>
         );
     }
 
     const DishDetail = (props) => {
 
-        console.log('DishDetail component render invoked');
-
         if(props.dish != null)
-            return(
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                            <RenderDish dish={props.dish} />
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                            <RenderComments comments={props.dish.comments} />
-                        </div>
-                    </div>
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
                 </div>
-            );
+                <div className="row">
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={props.comments} />
+                </div>
+            </div>
+        );
         else
             return (
                 <div></div>
