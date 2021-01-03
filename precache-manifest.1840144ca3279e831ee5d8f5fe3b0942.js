e7f279de805be216ec38b7d0d0c4356c053dc096
tilak30 <tilakbhansali3009@gmail.com>
2021-01-04 03:36:28 +0530

    Updates

---
+++ precache-manifest.1840144ca3279e831ee5d8f5fe3b0942.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a04a9ae29e9ae69e13017948155779",
+    "url": "/restaurant-website-using-react-and-redux/index.html"
+  },
+  {
+    "revision": "9afab4ec7ca66ecb8072",
+    "url": "/restaurant-website-using-react-and-redux/static/css/2.08553793.chunk.css"
+  },
+  {
+    "revision": "22124104cf1ed73609d2",
+    "url": "/restaurant-website-using-react-and-redux/static/css/main.a37c2842.chunk.css"
+  },
+  {
+    "revision": "9afab4ec7ca66ecb8072",
+    "url": "/restaurant-website-using-react-and-redux/static/js/2.f2275e03.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/restaurant-website-using-react-and-redux/static/js/2.f2275e03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22124104cf1ed73609d2",
+    "url": "/restaurant-website-using-react-and-redux/static/js/main.65ee78df.chunk.js"
+  },
+  {
+    "revision": "85792ca36d5200a18fff",
+    "url": "/restaurant-website-using-react-and-redux/static/js/runtime-main.93e7eaed.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/restaurant-website-using-react-and-redux/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/restaurant-website-using-react-and-redux/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/restaurant-website-using-react-and-redux/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/restaurant-website-using-react-and-redux/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/restaurant-website-using-react-and-redux/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
