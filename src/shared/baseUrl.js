69bdb27a511688a40b7ccd0c557898a96a6c4ed1
tilak30 <tilakbhansali3009@gmail.com>
2021-01-04 18:21:15 +0530

    commit

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export const baseUrl = 'https://my-json-server.typicode.com/tilak30/restaurant-website-using-react-and-redux/';
-=======
-export const baseUrl = 'https://my-json-server.typicode.com/tilak30/restaurant-website-using-react-and-redux/';
->>>>>>> 5b37e30fbb156cf01b093d0a195992c4def4f02c
