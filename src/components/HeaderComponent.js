a3ab1bb701d92808fbc6c9bf3554b3d7b5a64a2d
tilak30 <tilakbhansali3009@gmail.com>
2021-01-04 22:44:22 +0530

    images

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -3,6 +3,7 @@ import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem,
     Button, Modal, ModalHeader, ModalBody,
     Form, FormGroup, Input, Label } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+import logo from './images/logo.png';
   const bannerImg = {
     backgroundSize: '100%',
     backgroundImage: 'url(assets/images/test.png)',
@@ -45,7 +46,7 @@ class Header extends Component {
                 <Navbar dark expand="md">
                     <div className="container-fluid">
                         <NavbarToggler onClick={this.toggleNav} />
-                        <NavbarBrand className="mr-auto" href="/"><img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' /></NavbarBrand>
+                        <NavbarBrand className="mr-auto" href="/"><img src={logo} height="30" width="41" alt='Ristorante Con Fusion' /></NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
                             <NavItem>
