5b37e30fbb156cf01b093d0a195992c4def4f02c
Tilak Bhansali <60619989+tilak30@users.noreply.github.com>
2021-01-04 18:01:51 +0530

    Update baseUrl.js

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'https://my-json-server.typicode.com/tilak30/restaurant-website-using-react-and-redux';
+export const baseUrl = 'https://my-json-server.typicode.com/tilak30/restaurant-website-using-react-and-redux/';
